988c77d0fc248b1024f9e9f9129f3db84cdc7af3
Suxing Liu <suxingliu@gmail.com>
2024-02-28 09:40:55 -0500

    Update plantit_pipeline.sh
    try to fix plantit pull error

--- plantit_pipeline.sh
+++ plantit_pipeline.sh
@@ -3,6 +3,6 @@
 #python3 /opt/smart/core/color_seg.py -p $INPUT
 #python3 /opt/smart/core/trait_extract_parallel_demo.py -p $INPUT -m $MULTIPLE -l
 
-python3 /opt/smart/core/trait_extract_parallel_demo.py -p $INPUT -l
+python3 /opt/smart/core/trait_extract_parallel_demo.py -p $INPUT 
 
 
