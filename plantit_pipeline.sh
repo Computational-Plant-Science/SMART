5e802f1c45226964d229688aa0cddc9bf78a7693
lsx1980 <suxingliu@gmail.com>
2024-02-28 14:46:36 -0500

    update yaml file

--- plantit_pipeline.sh
+++ plantit_pipeline.sh
@@ -3,7 +3,7 @@
 #python3 /opt/smart/core/color_seg.py -p $INPUT
 #python3 /opt/smart/core/trait_extract_parallel_demo.py -p $INPUT -m $MULTIPLE -l
 
-python3 /opt/smart/core/trait_extract_parallel_demo.py -p $INPUT -ft $FILETYPE -s $COLOR-SPACE -c $CHANNELS -n $NUM_CLUSTERS -min $MIN_SIZE -min $MAX_SIZE -md $MIN_DIST -da $DIAGONAL -cc $CUE_COLOR -cl $CUE_LOC -ob $OUT_BOUNDARY
+python3 /opt/smart/core/trait_extract_parallel_demo.py -p $INPUT -s $COLOR-SPACE -c $CHANNELS -n $NUM_CLUSTERS -min $MIN_SIZE -min $MAX_SIZE -md $MIN_DIST -da $DIAGONAL -cc $CUE_COLOR -cl $CUE_LOC -ob $OUT_BOUNDARY
 
 
 
