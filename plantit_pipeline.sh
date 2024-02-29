e6201d3314fabd8ed4add6f19b8402533ca13c24
lsx1980 <suxingliu@gmail.com>
2024-02-29 18:48:05 -0500

    debug plantit yaml

--- plantit_pipeline.sh
+++ plantit_pipeline.sh
@@ -2,7 +2,7 @@
 
 #python3 /opt/smart/core/color_seg.py -p $INPUT
 
-python3 /opt/smart/core/trait_extract_parallel_demo.py -p $INPUT -ft $FILETYPES
+python3 /opt/smart/core/trait_extract_parallel_demo.py -p $INPUT --filetype $FILETYPE
 
 #python3 /opt/smart/core/trait_extract_parallel_demo.py -p $INPUT -o $OUTPUT --color_space $COLOR_SPACE --channels $CHANNELS --num_clusters $NUM_CLUSTERS --min_size $MIN_SIZE --max_size $MAX_SIZE --min_dist $MIN_DIST --diagonal $DIAGONAL 
 
