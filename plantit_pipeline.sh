391078fc63e1c1f693ee4cc963a22210ae6e9cb3
lsx1980 <suxingliu@gmail.com>
2024-02-29 10:46:42 -0500

    change input and output to debug plantit yaml

--- plantit_pipeline.sh
+++ plantit_pipeline.sh
@@ -6,5 +6,3 @@
 python3 /opt/smart/core/trait_extract_parallel_demo.py -p $INPUT -o $OUTPUT --color_space $COLOR_SPACE --channels $CHANNELS --num_clusters $NUM_CLUSTERS --min_size $MIN_SIZE --max_size $MAX_SIZE --min_dist $MIN_DIST --diagonal $DIAGONAL 
 
 
-
-
